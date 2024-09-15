65f1e6c9c459259918228e77f6142775132211d8
aaron218 <12155829@qq.com>
2024-09-16 00:03:06 +0800

    init

--- src/main/java21/nf/vt/VTDemo.java
+++ src/main/java21/nf/vt/VTDemo.java
@@ -17,18 +17,21 @@ public class VTDemo {
         }
     }
 
-
     public static void main(String[] args) {
         Thread thread = Thread.ofVirtual().unstarted(new Fn());
-        Thread thread1 = Thread.ofPlatform().start(fn);
+        Thread thread1 = Thread.ofVirtual().unstarted(fn);
+//        for (int i=0;i<100;i++){
+//            Thread t = Thread/
+//        }
         thread.start();
+        thread1.start();
         //join到当前线程上，否则可能会由于主线程先退出而导致无法显示内容
+        System.out.println("Start");
         try {
             thread.join();
             thread1.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
